4fd13763086c643fcf16918e174b1ab0d6458895
2020002960 <2020002960@hanyang.ac.kr>
2020-09-17 21:49:37 +0900

    3 makefile & 3-2-1 linkedlist modified

--- 3-2-1/main.c
+++ 3-2-1/main.c
@@ -8,20 +8,45 @@ typedef struct person{
 } Person;
 
 void addPerson(Person* header, char* name, int age) {
-    char*c=(char*)malloc((sizeof (char)) * 10);
+    Person* pp = header;
+    while (pp->next != NULL) {
+        pp = pp->next;
+    }
+    Person* newNode = (Person*) malloc(sizeof(Person));
+    
+    pp->next = newNode;
+    
+    char * c=(char*)malloc((sizeof (char)) * strlen(name));
     strcpy(c, name);
-    header->name = c;
-    header->age = age;
-    header->next = NULL;
+    newNode->name = c;
+    newNode->age = age;
+    newNode->next = NULL;
 }
 
 void increaseAge(Person* header) {
-    ++(header->age);
+    Person* pp = header->next;
+    while(1) {
+        ++(pp->age);
+        if (pp->next == NULL) break;
+        pp = pp->next;
+    }
 }
 
+void delete(Person* header) {
+    Person* pp = header->next;
+    while(1) {
+        printf("Name:%s, Age:%d\n", pp->name, pp->age);
+        free(pp->name);
+        if(pp->next == NULL) break;
+        Person* temp = pp->next;
+        free(pp);
+        pp = temp;
+    }
+}
 int main() {
-    Person* p = (Person*)malloc(sizeof(Person));
-    Person* now = p;
+    Person* head = (Person*)malloc(sizeof(Person));
+    head-> next = NULL;
+
     while (1) {
         int n;
         scanf("%d", &n);
@@ -31,21 +56,12 @@ int main() {
         for(int i= 0; i < n; ++i) {
             char name[10];
             int age;
-            now->next = (Person*)malloc(sizeof(Person));
-            now = now->next;
             scanf("%s%d", name, &age);
-            addPerson(now, name, age);
+            addPerson(head, name, age);
         }
     }
-    now = p->next;
-    while (now != NULL) {
-        increaseAge(now);
-        printf("Name:%s, Age:%d\n", now->name, now->age);
-        free(now->name);
-        Person * next = now->next;
-        free(now);
-        now = next;
-    }
+    increaseAge(head);
+    delete(head);
     return 0;
 }
 
